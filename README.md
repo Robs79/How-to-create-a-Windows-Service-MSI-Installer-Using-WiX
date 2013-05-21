99f8865410b8878da5f86508d9f0ea7ccd60a6e6
Lucifer79 <robert.sindall@gmail.com>
2013-05-21 07:53:46 +0100

    First Check In

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to create a Windows Service MSI Installer Using WiX
+
+This example shows you how to install a Windows Service using a WiX MSI Installer
+
+## References
+
+ - [Download Wix Toolset](http://wix.codeplex.com/releases/view/99514)
---
+++ WindowsService/MyWindowsService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public partial class MyWindowsService : ServiceBase
+    {
+        public MyWindowsService()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("Acme.WindowsService"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("Acme.WindowsService", "Application");
+            }
+            eventLog1.Source = "Acme.WindowsService";
+            eventLog1.Log = "Application";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In onStop.");
+        }
+    }
+}
